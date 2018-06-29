2b040c932e6dfe0a969a18fa5034682b93fd22f5
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-06-29 11:50:20 +0300

    first commit

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,88 @@
+<template>
+<div>
+    <h1>{{ formTitle }}{{title}}</h1>
+    <div><br/><button v-on:click="showForm()" style="background:blue; color:white;">Add New Task</button></div>
+    <div>
+        <div v-show="isEditing">
+            <h2>Adding a task</h2>
+
+                    <div>
+                        Task title:
+                    </div>
+                    <div>
+                        <input type="text" name="title" v-model="taskTitle" />
+                    </div>
+
+                    <div>
+                        Task name:
+                    </div>
+                    <div>
+                        <input type="tex" name="taskname" v-model="taskName" />
+                    </div>
+
+                    <div>
+                        <button v-on:click="closeForm()" >close</button>&nbsp;&nbsp;&nbsp;
+                        <button v-on:click="sendForm()" type="button">submit</button>
+                    </div>
+
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+    export default {
+        props:[
+            "title"
+        ],
+
+        data(){
+            return{
+                formTitle:"Add New Task",
+                count: 0,
+                secondname :"",
+                taskTitle:"",
+                taskName:"",
+                date: Date(),
+                isEditing: false,
+                todos:[]
+
+            }
+        },
+        methods:{
+            showForm(){
+                this.isEditing = true;
+            },
+            closeForm(){
+                this.isEditing = false;
+            },
+            sendForm (){
+                let id = this.todos.length +1;
+
+                if(this.taskTitle!='' && this.taskName!='') {
+                    const date = new Date();
+                    const done = false;
+
+                    let newTodos = {id:id,title:this.taskTitle,Task:this.taskName,date:this.date,done:this.done};
+                    this.todos.push(newTodos);
+                    this.newTodos = [];
+                    console.log(newTodos);
+                    this.$emit("taskAdded",newTodos);
+                    console.log(this.$root.$emit("taskAdded",newTodos));
+                }else{
+                    console.log(newTodos);
+                }
+
+            },
+        }
+
+    }
+
+</script>
+
+<style>
+    table{
+        align:center;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <div>
+            <h2 style="">{{ apptitle }}</h2>
+
+            <div v-on:delete-task="deleteTask" v-for="todo in todos" :key="todo.id">
+                <div>{{ todo.title }} &nbsp;&nbsp;&nbsp;{{todo.Task}} &nbsp;&nbsp;&nbsp;<button v-on:click="deleteTask(todo)" >X</button></div>
+
+            </div>
+            <todo-form :title="title" ></todo-form>
+
+
+        </div>
+
+
+    </div>
+
+</template>
+
+<script type="text/javascript">
+
+
+    import Vue from 'vue';
+    import AddTodoForm from './AddTodoForm.vue'
+
+    export default {
+        components:{
+            'todo-form':AddTodoForm
+        },
+
+
+        data(){
+            return{
+                title:"Todo Application with VueJS",
+                count: 0,
+                firstname:"",
+                secondname :"",
+                taskTitle:"",
+                taskName:"",
+                date: Date(),
+                isEditing: false,
+                todos:[{
+                    id:1,
+                    title:'Todo A',
+                    Task:'project A',
+                    date:'jun 30 2018',
+                    done:false
+                },
+                    {
+                        id:2,
+                        title:'Todo B',
+                        Task:'project B',
+                        date:'jun 30 2018',
+                        done:false
+                    },{
+                        id:3,
+                        title:'Todo C',
+                        Task:'project C',
+                        date:'jun 30 2018',
+                        done:false
+                    }],
+
+                apptitle:"Simple to do List",
+                databinding:"Two way data binding",
+
+            }
+
+        },
+        created() {
+            this.$root.$on('taskAdded', (text) => {
+               // this.title = text;
+                this.todos.push(text);
+            })
+        },
+        methods:{
+            deleteTask(todo){
+                console.log(todo);
+                this.todos.pop(todo);
+            }
+
+        },
+        computed:{
+
+        }
+    }
+
+</script>
